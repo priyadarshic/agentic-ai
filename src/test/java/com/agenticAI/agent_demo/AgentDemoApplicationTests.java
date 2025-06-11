9834082a9a0566b720fcdeddd27c86070679f671
priyadarshic <priyadarshi.c@gmail.com>
2025-06-11 16:16:26 +0530

    Agentic AI using Google ADK API

---
+++ src/test/java/com/agenticAI/agent_demo/AgentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenticAI.agent_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
